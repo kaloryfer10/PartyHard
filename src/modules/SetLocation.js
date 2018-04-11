ee6ee33b0ab7b60d6273865f80bfc58c0b9fa3f0
kaloryfer10 <michail.meczynski@gmail.com>
2018-04-12 00:04:19 +0200

    SetLocation added
    SetLocation added

--- src/modules/SetLocation.js
+++ src/modules/SetLocation.js
@@ -4,17 +4,114 @@ import {
   Text, 
   View,
   Button,
-  Image,
+  Dimensions,
+  Switch,
   Alert,
 } from 'react-native';
 
-import MapImage from '../../assets/map.jpg';
+import MapView, { Marker } from 'react-native-maps';
+
+const {width, height} = Dimensions.get('window');
+const SCREEN_WIDTH = width;
+const SCREEN_HEIGHT = height;
+const ASPECT_RATIO = width / height;
+const LATITUDE_DELTA = 0.2922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+
 
 class MapContainer extends React.Component {
-  render () {
+  constructor (props) {
+    super (props);
+    
+    this.state = {
+      initialPosition: {
+        latitude: 0,
+        longitude: 0,
+        latitudeDelta: 0,
+        longitudeDelta: 0,
+      },
+      markerPosition: {
+        latitude: 0,
+        longitude: 0,
+      },
+      city: null,
+    }
+  }
+
+  returnCity () {
+    
+  }
+  
+  componentDidMount () {
+    navigator.geolocation.getCurrentPosition((position) => {
+      var lat = parseFloat(position.coords.latitude)
+      var longi = parseFloat(position.coords.longitude)
+      
+      var initialRegion = {
+        latitude: lat,
+        longitude: longi,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA
+      }
+      this.setState({ initialPosition: initialRegion });
+      this.setState({ markerPosition: initialRegion });
+
+      fetch('http://maps.googleapis.com/maps/api/geocode/json?latlng=50.2667461,18.6565925&sensor=true')
+        .then((response) => response.json())
+        .then((responseJSON) => {
+          if(responseJSON.status === 'OK') {
+            this.setState({city: responseJSON.results[2].formatted_address})
+            console.log(responseJSON.results[2].formatted_address);
+          } else {
+            console.error('Request error')
+          }
+          
+        })
+        .catch((error) =>{
+          console.error(error);
+      })
+      console.log(this.state.city);
+      
+    }, 
+    (error) => alert(JSON.stringify(error)),
+    {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000})
+    /*
+    this.watchID = navigator.geolocation.watchPosition((position) => {
+      var lat = parseFloat(position.coords.latitude)
+      var longi = parseFloat(position.coords.longitude)
+
+      var lastRegion = {
+        latitude: lat,
+        longitude: longi,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA
+      }
+      this.setState({ initialPosition: lastRegion });
+      this.setState({ markerPosition: lastRegion });
+    });
+    */
+  }
+
+
+  componentWillUnmount () {
+    navigator.geolocation.clearWatch(this.watchID);
+  }
+
+  render() {
     return (
       <View style={styles.mapContainer}>
-        <Image source={MapImage} />
+        <MapView
+          style={styles.map}
+          region={this.state.initialPosition} >
+          <MapView.Marker
+            coordinate={this.state.markerPosition} >
+            <View style={styles.markerRadius}>
+              <View style={styles.marker}>
+              </View>
+            </View>
+          </MapView.Marker>
+        </MapView>
+        <Text style={styles.cityName}>{this.state.city}</Text>
       </View>
     );
   }
@@ -26,14 +123,16 @@ class ButtonsContainer extends React.Component {
       <View style={styles.buttonsContainer}>
         <View style={styles.button}>
           <Button
-            onPress={()=>{Alert.alert('GPS button click.')}}
+            onPress={() => {
+              console.log('Set location using GPS button clicked.');
+            }}
             title='Set location using GPS'
             color='#D32F2F'
           />
         </View>
         <View style={styles.button}>
           <Button
-            onPress={()=>{Alert.alert('Choose town button click.')}}
+            onPress={()=>{console.log('Choose town button clicked.')}}
             title='Choose town'
             color='#3F51B5'
           />
@@ -63,6 +162,36 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  map: {
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    position: 'absolute',
+  },
+  markerRadius: {
+    height: 50,
+    width: 50,
+    borderRadius: 50 / 2,
+    overflow: 'hidden',
+    backgroundColor: 'rgba(0, 122, 255, 0.1)',
+    borderWidth: 1,
+    borderColor: 'rgba(0, 112, 255, 0.3)',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  marker: {
+    height: 20,
+    width: 20,
+    borderWidth: 3,
+    borderColor: '#FFFFFF',
+    borderRadius: 20 / 2,
+    overflow: 'hidden',
+    backgroundColor: '#007AFF',
+  },
+  cityName: {
+    alignItems: 'flex-end',
+  },
   buttonsContainer: {
     flex: 1,
     backgroundColor: '#262626',
@@ -73,5 +202,5 @@ const styles = StyleSheet.create({
     margin: 10,
     padding: 5,
     width: 250,
-  }
+  },
 });
